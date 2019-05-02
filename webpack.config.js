6c61b4cb07c48c9824473a8dbee5806ee5d373b5
Arshavin86 <sevost.andrey@gmail.com>
2019-05-02 10:50:40 -0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+};
