15cee35c41c28248811ef8dc8783d29453ec8613
Arshavin86 <sevost.andrey@gmail.com>
2019-05-16 09:02:06 -0700

    Create pages and fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+// This is the Link API
+import Link from 'next/link';
+import Header from '../components/Header';
+import Layout from '../components/MyLayout.js';
+
 
 const server = 'http://localhost:3001/api/games/date/';
 
 export default function Index () {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
           (async() => {
             let date = '2018-10-17';
             try {
-              var response = await fetch(server + date);
-              var data = await response.json();
-              console.log(data);
+              const response = await fetch(server + date);
+              const res = await response.json();
+              console.log(res);
+              setData (res);
+              setLoading(false); 
             } catch (e) {
               console.log(e);
             }
@@ -18,8 +27,19 @@ export default function Index () {
       }, []);
 
     return (
-        <div>
+        <Layout>
           <p>I'm rendering because of Next!</p>
-        </div>
+          {loading ? (
+            "Loading..."
+          ) : (
+            <ul>
+              {data.map(item => (
+                <li key={item.id}>
+                  <a>{item.bestplayer1}</a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Layout>
       );
 }
---
+++ pages/players.js
@@ -0,0 +1,10 @@
+import Layout from '../components/MyLayout.js';
+
+export default function Players () {
+
+    return (
+        <Layout>
+            <p>Here come our players!</p>
+        </Layout>
+      );
+}
