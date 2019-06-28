28b56ad2351152a051f7d86148f7c9aa3b6ef29b
Arshavin86 <sevost.andrey@gmail.com>
2019-06-27 21:42:01 -0700

    Create individuale folders for seasons page components for quiker reference and work with it

---
+++ components/seasons/Table.js
@@ -0,0 +1,57 @@
+
+import style from 'styled-components';
+import Table_body from './Table_body';
+
+
+const Table_main = style.table`
+    overflow-x: auto;
+    border-collapse: separate;
+    color: #333;
+    font-family: Roboto,sans-serif;
+    font-size: 14px;
+    line-height: 16px;
+    margin: 0;
+    width: 100%;
+    border-top: 2px solid #9b9b9b;
+    border-bottom: 1px solid #e6e8ea;
+`;
+
+const Table_head = style.thead`
+    overflow-x: auto;
+    border-collapse: separate;
+    color: #333;
+    font-family: Roboto,sans-serif;
+    font-size: 14px;
+    line-height: 16px;
+    margin: 0;
+    width: 100%;
+`;
+
+const Table = props => {
+
+    return (
+        <Table_main>
+            <Table_head>
+                <tr>
+                    <th>Rk</th>
+                    <th></th>
+                    <th>Pos</th>
+                    <th>G</th>
+                    <th>MP</th>
+                    <th>FG%</th>
+                    <th>TRB</th>
+                    <th>AST</th>
+                    <th>STL</th>
+                    <th>BLK</th>
+                    <th>TOV</th>
+                    <th>PTS</th>
+                </tr>
+            </Table_head>
+            <Table_body/>
+        </Table_main>
+    )
+    
+}
+
+export default Table;
+
