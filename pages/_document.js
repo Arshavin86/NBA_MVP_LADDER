27515a4b211319f9aecfb3d411e1e59fc68da67c
Arshavin86 <sevost.andrey@gmail.com>
2019-08-05 17:54:31 -0700

    Add links for favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,9 +14,16 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>NBA MVP Ladder</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png"></link>
+          <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png"></link>
+          <link rel="manifest" href="../site.webmanifest"></link>
+          <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5"></link>
+          <meta name="msapplication-TileColor" content="#da532c"></meta>
+          <meta name="theme-color" content="#ffffff"></meta>
           {this.props.styleTags}
         </Head>
-        <body style={{ margin: '0' }}>
+        <body style={{ margin: '0' }} >
           <Main />
           <NextScript />
         </body>
