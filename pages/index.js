165c5c7f9d7a12284d7a7adf072c2e7ff09e5b3a
Arshavin86 <sevost.andrey@gmail.com>
2019-08-20 13:44:31 -0700

    Add propTypes and eslint related changes

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import {useState, useEffect} from 'react';
-import Calendar_nav from '../components/index/side/Calendar_nav';
-import Layout from '../components/MyLayout';
-import Scoreboard_wrapper from '../components/index/side/Scoreboard_wrapper';
+/* eslint-disable func-names */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable consistent-return */
+import { useState, useEffect } from 'react';
 import style from 'styled-components';
 import fetch from 'isomorphic-unfetch';
+import PropTypes from 'prop-types';
+import CalendarNav from '../components/index/side/CalendarNav';
+import Layout from '../components/MyLayout';
+import Scoreboard_wrapper from '../components/index/side/Scoreboard_wrapper';
 import ApiContext from '../components/Context';
 import MainBoard from '../components/index/main/MainBoard';
 import Server from '../components/Server';
 
-const server = Server.server;
+
+const { server } = Server;
 
 const Container1 = style.div`
   display: flex; 
@@ -53,33 +59,33 @@ const Main = style(Scoreboard_bottom)`
   background: #fefefe;
 `;
 
-const formatDate = date => {
-   
-  let d = date ? new Date(date) : new Date(),
-  month = '' + (d.getMonth() + 1),
-  day = '' + d.getDate(),
-  year = d.getFullYear();
+const formatDate = (date) => {
+  const d = date ? new Date(date) : new Date();
+  let month = `${d.getMonth() + 1}`;
+  let day = `${d.getDate()}`;
+  const year = d.getFullYear();
 
   if (month.length < 2) {
-    month = '0' + month;
+    month = `0${month}`;
   }
   if (day.length < 2) {
-    day = '0' + day;
+    day = `0${day}`;
   }
   return [year, month, day].join('-');
-}
+};
 
-const Index = props => {
-  const [data, setData] = useState(props.games);
-  const [date, setDate] = useState(props.date);
+const Index = (props) => {
+  const { games, todaysDate } = props;
+  const [data, setData] = useState(games);
+  const [date, setDate] = useState(todaysDate);
   const [news, setNews] = useState('nba');
   const [videos, setVideo] = useState(null);
-  const [videosOn, setMain] = useState (false);
+  const [videosOn, setMain] = useState(false);
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       try {
-	      const response = await fetch (server + 'news/' + news);
+        const response = await fetch(`${server}news/${news}`);
         // const response = await fetch (server + 'videos/' + query);
         const json = await response.json();
         setNews(json);
@@ -92,86 +98,91 @@ const Index = props => {
     })();
   }, []);
 
-  const handleDateChange = async date => {
+  const handleDateChange = async (date) => {
     const ISODate = formatDate(date);
     let json;
-    //console.log('ISODate:', ISODate);
-    setDate (ISODate);
+    // console.log('ISODate:', ISODate);
+    setDate(ISODate);
     try {
-      const response = await fetch(server + 'games/date/' + ISODate);
+      const response = await fetch(`${server}games/date/${ISODate}`);
       if (response.status === 500) {
         json = 'No games were played on this day';
       } else {
         json = await response.json();
       }
-      setData (json);
+      setData(json);
     } catch (e) {
       console.warn(e);
     }
-  } 
+  };
 
-  const handleVideoChange = async query => {
+  const handleVideoChange = async (query) => {
     try {
-	      const response = await fetch (server + 'videos/' + query + ' ' + date);
-        const json = await response.json();
-       // console.log('Youtube data on FE:', json);
-        setVideo(json);
-        setMain(true);
+	    const response = await fetch(`${server}videos/${query} ${date}`);
+      const json = await response.json();
+      // console.log('Youtube data on FE:', json);
+      setVideo(json);
+      setMain(true);
     } catch (e) {
       console.warn(e);
     }
-  }
-  
+  };
+
   return (
     <Layout>
       <Container1>Here could be your advertisement </Container1>
       <Container2>
         <Scoreboard>
           <Scoreboard_nav>
-            <ApiContext.Provider value = {[date, handleDateChange]}>
-              <Calendar_nav/>
+            <ApiContext.Provider value={[date, handleDateChange]}>
+              <CalendarNav />
             </ApiContext.Provider>
-          </Scoreboard_nav>  
-          <Scoreboard_bottom> 
-            <ApiContext.Provider value = {[data, handleVideoChange]}>
-              <Scoreboard_wrapper/>
+          </Scoreboard_nav>
+          <Scoreboard_bottom>
+            <ApiContext.Provider value={[data, handleVideoChange]}>
+              <Scoreboard_wrapper />
             </ApiContext.Provider>
           </Scoreboard_bottom>
         </Scoreboard>
         <Main>
-          <ApiContext.Provider value = {[videos, news, videosOn, date]}>
-            <MainBoard/>
+          <ApiContext.Provider value={[videos, news, videosOn, date]}>
+            <MainBoard />
           </ApiContext.Provider>
         </Main>
-      </Container2> 
+      </Container2>
     </Layout>
   );
-}
+};
+
+Index.propTypes = {
+  games: PropTypes.object,
+  todaysDate: PropTypes.string,
+};
 
 Index.getInitialProps = async function () {
   // let date = formatDate('2019-06-15');
   // let date = formatDate('2019-07-12');
-  let date = formatDate();
+  const date = formatDate();
   let json;
-  //console.log('DATE in props: ', date);
+  // console.log('DATE in props: ', date);
 
   try {
-    const res = await fetch(server + 'games/date/' + date);
-   // console.log('res status on FE:', res.status);
+    const res = await fetch(`${server}games/date/${date}`);
+    // console.log('res status on FE:', res.status);
     if (res.status === 500) {
       json = 'No games were played on this day';
     } else {
       json = await res.json();
-     // console.log('json!!!!!', json);
+      // console.log('json!!!!!', json);
     }
     return {
       games: json,
-      date: date,
+      todaysDate: date,
       // query: json[0].id
-    }
+    };
   } catch (e) {
     console.warn(e);
   }
-}
+};
 
 export default Index;
