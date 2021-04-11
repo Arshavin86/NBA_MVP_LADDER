7be34a3c4c2e0f18a66042a7f2e5427d9d8936f9
Arshavin86 <sevost.andrey@gmail.com>
2021-04-10 23:40:19 -0700

    fix: env. variables are undefined in the config.js

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 module.exports = {
-    serverHost: process.env.SERVER_HOST || 'http://localhost:3001',
-    X_RapidAPI_Key: process.env.X_RapidAPI_Key,
-    databaseConfig: {
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
-      user: process.env.DB_USER,
-      database: process.env.DB || 'nba',
-      password: process.env.DB_PASSWORD
-    },
-    newsApiKey: process.env.NEWS_API_KEY,
-    youtube: {
-      API_Key: process.env.YOUTUBE_API_KEY,
-      CLIENT_ID: process.env.YOUTUBE_CLIENT_ID,
-      CLIENT_SECRET: process.env.YOUTUBE_CLIENT_SECRET,
-      REDIRECT_URL: process.env.YOUTUBE_REDIRECT_URL || 'http://localhost:3000'
-    }
+  serverHost: process.env.SERVER_HOST || 'http://localhost:3001',
+  X_RapidAPI_Key: process.env.X_RapidAPI_Key,
+  databaseConfig: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    database: process.env.DB || 'nba',
+    password: process.env.DB_PASSWORD
+  },
+  newsApiKey: process.env.NEWS_API_KEY,
+  youtube: {
+    API_Key: process.env.YOUTUBE_API_KEY,
+    CLIENT_ID: process.env.YOUTUBE_CLIENT_ID,
+    CLIENT_SECRET: process.env.YOUTUBE_CLIENT_SECRET,
+    REDIRECT_URL: process.env.YOUTUBE_REDIRECT_URL || 'http://localhost:3000'
   }
+}
--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
   cssLoaderOptions: {
     url: false
+  },
+  webpack: (config, options) => {
+    config.node = { fs: 'empty' }
+    return config
   }
 })
